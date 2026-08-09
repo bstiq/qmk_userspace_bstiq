9156598ffee79ca40012b352e9d9c151c63a0b3d
Quentin <bstkbd@gmail.com>
2026-08-09 21:16:46 +0200

    even more endurance

--- modules/bastardkb/argos/config.h
+++ modules/bastardkb/argos/config.h
@@ -29,9 +29,9 @@
 #endif
 
 // More available live storage! Useful for tap dances and RGB
-#define WEAR_LEVELING_BACKING_SIZE 131072
-// better read write endurance
-#define WEAR_LEVELING_LOGICAL_SIZE (WEAR_LEVELING_BACKING_SIZE / 4)
+// Also, better read write endurance
+#define WEAR_LEVELING_LOGICAL_SIZE 65536
+#define WEAR_LEVELING_BACKING_SIZE (WEAR_LEVELING_LOGICAL_SIZE * 4)
 
 #define SPLIT_TRANSACTION_IDS_KB RPC_ID_RGB_SYNC
 
