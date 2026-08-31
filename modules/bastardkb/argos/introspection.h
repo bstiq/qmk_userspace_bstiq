abc578957b6845a47d3ced864aa253d57b097405
Quentin <bstkbd@gmail.com>
2026-08-31 20:41:08 +0200

    cleaner now

--- modules/bastardkb/argos/introspection.h
+++ modules/bastardkb/argos/introspection.h
@@ -6,22 +6,25 @@ combo_t key_combos[ARGOS_COMBO_ENTRIES];
 tap_dance_action_t tap_dance_actions[ARGOS_TAP_DANCE_ENTRIES];
 
 // leave space for a LOT of pointing device codes, 
-// to make mode management easier in the pointing device module
+// to make mode management easier in the pointing device module 
+// and give ourselves some breathing room for future features
+// so we start at 0x7E60
 
-// RANGE NOT USED - FOR LO capability
-#define LO_0 0x7E60 // sync with BK_LO
-#define LO_LAST 0x7E6A // not used, only for delimitation
+// momentary switch layer exclusive - 32 layers max
+#define BK_MOMENTARY_EXCLUSIVE 0x7E60 
+#define BK_MOMENTARY_EXCLUSIVE_MAX 0x7E7F
+#define MOE(layer) (BK_MOMENTARY_EXCLUSIVE | ((layer) & 0x1F))
+#define BK_MOMENTARY_EXCLUSIVE_GET_LAYER(kc) (QK_MOMENTARY_GET_LAYER(kc))
 
-// RANGE NOT USED - FOR LTO capability
-#define LTO_0 0x7E6B // sync with BK_LTO
-#define LTO_LAST 0x7E75 // not used, only for delimitation
+// L-ayer M-od E-xclusive: Momentary switch layer with modifiers active - 16 layer max
+#define BK_LAYER_MOD_EXCLUSIVE 0x7E80
+#define BK_LAYER_MOD_EXCLUSIVE_MAX 0x7E9F
+#define LME(layer, mod) (BK_LAYER_MOD_EXCLUSIVE | (((layer) & 0xF) << 5) | ((mod) & 0x1F))
+#define BK_LAYER_MOD_EXCLUSIVE_GET_LAYER(kc) (QK_LAYER_MOD_GET_LAYER(kc))
+#define BK_LAYER_MOD_EXCLUSIVE_GET_MODS(kc) (QK_LAYER_MOD_GET_MODS(kc))
 
-// Custom layer keys
-#define BK_LO 0x7E60 // sync with ALO_0
-#define BK_LTO 0x7E6B // sync with ALTO_0
+// momentary switch layer exclusive when held, key on tap
+// TODO
 
-// momentary switch layer, disable all other layers
-#define LO(layer) (BK_LO | ((layer) & 0x09)) // 10 layers max (0-9)
-
-// momentary switch layer on hold, disable all other layers
-#define LTO(layer, kc) (BK_LTO | (((layer) & 0xF) << 8) | ((kc) & 0xFF))
+// momentary switch layer exclusive with modifiers active when held, key on tap
+// TODO
